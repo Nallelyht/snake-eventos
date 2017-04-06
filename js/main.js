25a3ae7b6080c0b3e030bd7afb7b26e36bbd341e
Nallelyht <you@example.com>
2017-04-06 15:42:47 -0500

    se mueve la cabeza de la serpiente con restricciones y sin saltos extraños

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 
 var contenedor = document.getElementById("contenedor");
 var cabezaSerpiente = document.getElementById("cabezaSerpiente");
-var nuevaPosicion = 0;
+var izqDer = 0;
+var arrAba = 0;
 
 document.addEventListener("keyup",moverDiv);
     
@@ -13,32 +14,32 @@ function moverDiv(tecla){
    // console.log(teclaPresionada);
     
     if (teclaPresionada==39){
-        nuevaPosicion+=20;
-        cabezaSerpiente.style.marginLeft = nuevaPosicion+ "px";
+        izqDer+=20;
+        cabezaSerpiente.style.marginLeft = izqDer+ "px";
         
-        if (nuevaPosicion>480){
+        if (izqDer>480){
             alert("Game Over");
             cabezaSerpiente.style.marginLeft= "0px";
         }
     } else if (teclaPresionada==37){
-        nuevaPosicion-=20;
-        cabezaSerpiente.style.marginLeft = nuevaPosicion + "px";
-        if (nuevaPosicion<0){
+        izqDer-=20;
+        cabezaSerpiente.style.marginLeft = izqDer + "px";
+        if (izqDer<0){
             alert("Game Over");
             cabezaSerpiente.style.marginLeft= "0px";
         }
     } else if (teclaPresionada==40){
-        nuevaPosicion+=20;
-        cabezaSerpiente.style.marginTop = nuevaPosicion + "px";
-        if (nuevaPosicion>480){
+        arrAba+=20;
+        cabezaSerpiente.style.marginTop = arrAba + "px";
+        if (arrAba>480){
             alert("Game Over");
             cabezaSerpiente.style.marginTop= "0px";
         }
     } else if (teclaPresionada==38){
-        nuevaPosicion-=20;
-        cabezaSerpiente.style.marginTop = nuevaPosicion + "px";
-        console.log(nuevaPosicion);
-        if (nuevaPosicion<0){
+        arrAba-=20;
+        cabezaSerpiente.style.marginTop = arrAba + "px";
+        console.log(arrAba);
+        if (arrAba<0){
             alert("Game Over");
             cabezaSerpiente.style.marginTop= "0px";
         }
