e06278beaed56b7177662881b021cde3138e53df
Nallelyht <you@example.com>
2017-04-06 15:16:34 -0500

    se mueve la cabeza de arriba a abajo, izq a der, se ponen restricciones pero da saltos raros

--- js/main.js
+++ js/main.js
@@ -9,17 +9,40 @@ document.addEventListener("keyup",moverDiv);
 			
 function moverDiv(tecla){
     var teclaPresionada = tecla.keyCode;
-    console.log(nuevaPosicion);
-    console.log(teclaPresionada);
+    //console.log(nuevaPosicion);
+   // console.log(teclaPresionada);
     
     if (teclaPresionada==39){
         nuevaPosicion+=20;
         cabezaSerpiente.style.marginLeft = nuevaPosicion+ "px";
-        console.log(nuevaPosicion);
+        
+        if (nuevaPosicion>480){
+            alert("Game Over");
+            cabezaSerpiente.style.marginLeft= "0px";
+        }
     } else if (teclaPresionada==37){
         nuevaPosicion-=20;
         cabezaSerpiente.style.marginLeft = nuevaPosicion + "px";
+        if (nuevaPosicion<0){
+            alert("Game Over");
+            cabezaSerpiente.style.marginLeft= "0px";
+        }
+    } else if (teclaPresionada==40){
+        nuevaPosicion+=20;
+        cabezaSerpiente.style.marginTop = nuevaPosicion + "px";
+        if (nuevaPosicion>480){
+            alert("Game Over");
+            cabezaSerpiente.style.marginTop= "0px";
+        }
+    } else if (teclaPresionada==38){
+        nuevaPosicion-=20;
+        cabezaSerpiente.style.marginTop = nuevaPosicion + "px";
+        console.log(nuevaPosicion);
+        if (nuevaPosicion<0){
+            alert("Game Over");
+            cabezaSerpiente.style.marginTop= "0px";
+        }
+    } else {
+        alert("Juega solo con las flechas");
     }
-       
-
 }
